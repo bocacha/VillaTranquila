99ab69e885cf1848f18ff358c1b50e8e9ab49b8e
JavierArdiles <javierardilesreibel@gmail.com>
2021-09-27 17:10:37 -0300

    empieza home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
