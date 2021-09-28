b40042c27e415b6c8aa102b34c8b4c3b80eb0217
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2021-09-28 14:46:58 -0300

    formulario de contacto

--- client/src/components/Contacto/Contacto.jsx
+++ client/src/components/Contacto/Contacto.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import style from '../Contacto/Contacto.module.css'
+
 
 export default function Contacto(){
 
+    const [control, setControl] = useState({
+        name: '',
+        tel:'',
+        email:'',
+        query:''
+    })
+
+    const [error, setError] = useState(false);
+
+    const {name, tel, email, query} = control;
+
+    const changeControl = (e) =>{
+        setControl({
+            ...control,
+                [e.target.name]: e.target.value
+        })
+    }
+
+    const submitQuery = (e)=>{  
+        e.preventDefault();
+        if(name ===''|| tel=== ''|| email ==='' || query === ''){
+            setError(true)
+            return;
+        }
+        setError(false);
+    }
+
     return (
-        <div>
-            <form>
+        <div >
+           <form
+                 onSubmit={submitQuery} 
+                 className={style.form}
+            >
+                { error ? 
+                    <div className={style.Error}>
+                        <h3>Todos los campos son obligatorios</h3>
+                    </div>  
+                :  
+                    null}
+                <h2>Contacto</h2>
+                <p type="Nombre:"><input name="name" value={name} onChange={changeControl} placeholder="Nombre"></input></p>
+                <p type="Telefono:"><input name="tel"value={tel} onChange={changeControl} placeholder="Telefono"></input></p>
+                <p type="Email:"><input name="email"value={email} onChange={changeControl} placeholder="Email"></input></p>
+                <p type="Consulta:"><input name="query" value={query} onChange={changeControl} placeholder="Consulta"></input></p>
+                <button type="submit">Enviar</button>
                 
-            </form>
+        </form>
+
         </div>
+        
     )
 }
