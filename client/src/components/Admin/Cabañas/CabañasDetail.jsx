ceaeb099860f64e57b0349190c242038a121bc70
SpyraL-dev <81209349+SpyraL-dev@users.noreply.github.com>
2021-10-06 09:40:47 -0300

    mostrar ocultados

--- client/src/components/Admin/Cabañas/CabañasDetail.jsx
+++ client/src/components/Admin/Cabañas/CabañasDetail.jsx
@@ -30,22 +30,23 @@ export default function CabinsDetail({
     dispatch(removeCabains(obj));
     window.location.reload();
   }
+  const handleSubmitrestore = (ID)=>{
+    console.log('funcion', ID)
+    alert("su cabaña fue Eliminada con exito");
+    let obj = {id:ID}
+    dispatch(removeCabains(obj));
+    window.location.reload();
+  }
   return (
     <div className={styles.container}>
       <p><strong>Id:</strong>  {ID} </p>
-      <p><strong>Habitaciones:</strong> {Number}</p>
+      <p><strong>Numero de Cabaña:</strong> {Number}</p>
       <p><strong>Camas:</strong>  {Capacity}</p>
       <p><strong>Available:</strong>  {Available}</p>
       <p><strong>Price:</strong>  {Price}</p>
       <p><strong>Descripcion:</strong>  {Description}</p>
-      <p><strong>Coffe:</strong>  {Coffe?<span>si</span>:<span>no</span>}</p>
-      <p><strong>Microondas:</strong>  {Microondas?<span>si</span>:<span>no</span>}</p>
-      <p><strong>Calefaccion:</strong>  {Calefaccion?<span>si</span>:<span>no</span>}</p>
       <p><strong>Barbecue:</strong>  {Barbecue?<span>si</span>:<span>no</span>}</p>
       <p><strong> Wifi:</strong> {Wifi?<span>si</span>:<span>no</span>}</p>
-      <p><strong>Cleaning: </strong> {Cleaning?<span>si</span>:<span>no</span>}</p>
-      <p><strong>Refrigerator:</strong>  {Refrigerator?<span>si</span>:<span>no</span>}</p>
-      <p><strong>Stove:</strong>  {Stove?<span>si</span>:<span>no</span>}</p>
       <p><strong>Parking:</strong>  {Parking?<span>si</span>:<span>no</span>}</p>
       <div>
         <button onClick={()=>handleSubmitDelete(ID)}>Eliminar</button>
