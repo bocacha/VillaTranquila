43422b3e458e6818d52f05135c0f18b3ac4a612b
JavierArdiles <javierardilesreibel@gmail.com>
2021-10-12 15:41:19 -0300

    estilos registro y login, falta olvide contraseña

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch , useSelector} from "react-redux";
-import { Loguser, Logeduser,mailpassword } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import { Loguser, Logeduser, mailpassword } from "../../actions";
 import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
+import { ImCalendar } from 'react-icons/im';
 
 export default function Login() {
   const dispatch = useDispatch();
   const [user, setuser] = useState({ UserName: "", UserPassword: "" });
-  const [email, setEmail]= useState({Email:""})
+  const [email, setEmail] = useState({ Email: "" })
   const [on, setON] = useState(false)
   useEffect(() => {
     dispatch(Logeduser());
@@ -31,72 +32,83 @@ export default function Login() {
     dispatch(Loguser(user));
     // window.location.href='/'
   };
-  const goHome =()=>{
-    window.location.href='/reserva'
+  const goHome = () => {
+    window.location.href = '/reserva'
   }
-  const send=()=> {
-    dispatch(mailpassword(email.Email),dispatch)
+  const send = () => {
+    dispatch(mailpassword(email.Email), dispatch)
   }
-  let logeduser = useSelector ((state) => state.user);
-  if(logeduser===null){
-      logeduser = {}
-      logeduser.admin = false 
-      logeduser.token =false
+  let logeduser = useSelector((state) => state.user);
+  if (logeduser === null) {
+    logeduser = {}
+    logeduser.admin = false
+    logeduser.token = false
   }
   return (
-    <div className={styles.container}>
+    <div className={styles.login}>
       <Navbar />
-      {!logeduser.token ? (
-      <div className={styles.containerForm}>
-        <form>
-          <input
-            type="text"
-            placeholder="Nombre de usuario"
-            name="UserName"
-            value={user.UserName}
-            onChange={Handlechange}
-            className={styles.formInputs}
-          />
-          <input
-            type="password"
-            placeholder="Contraseña"
-            name="UserPassword"
-            value={user.UserPassword}
-            onChange={Handlechange}
-            className={styles.formInputs}
-          />
-          <button onClick={handleLogin} className={styles.btn}>Iniciar sesión</button>
-        </form>
-        <div>
-      <button className={styles.btn}onClick={()=>setON(true)} >Olvide mi usuario o contraseña</button>
-  <div>
-    {on ?(
-      <div>
-      Email:
-      <input
-       type="text"
-       placeholder="ejemplo@ejemplo.com"
-       name="Email"
-       value={email.Email}
-       onChange={HandlechangeE}
-       className={styles.formInputs} />
-       <button className={styles.btn} onClick={send}>enviar</button>
-      </div>
-    ):null}
-  </div>
-</div>
-      </div>
-       ) : (
-         <div className={styles.container}> 
-           <div className={styles.containerForm}>
-         <h4> Bienvenido/a  {logeduser.user}, vamos a   </h4>
-            <button className={styles.btn1}onClick={goHome} ><strong>Comenzar reserva</strong></button>                 
-         </div>
-           </div>
-           
+      <div className={styles.container}>
+        {!logeduser.token ? (
+          <div className={styles.containerForm}>
+            <div className={styles.title}><strong>Inicie sesión con su nombre de usuario y contraseña</strong></div>
+            <form>
+              <input
+                type="text"
+                placeholder="Nombre de usuario"
+                name="UserName"
+                value={user.UserName}
+                onChange={Handlechange}
+                className={styles.formInputs}
+              />
+              <input
+                type="password"
+                placeholder="Contraseña"
+                name="UserPassword"
+                value={user.UserPassword}
+                onChange={Handlechange}
+                className={styles.formInputs}
+              />
+              <button onClick={handleLogin} className={styles.btn}>Iniciar sesión</button>
+            </form>
+            <div>
+              <button
+                className={styles.btn}
+                onClick={() => setON(true)}
+              >
+                <u>Olvidé mi usuario o contraseña</u>
+              </button>
+              <div>
+                {on ? (
+                  <div>
+                    Email:
+                    <input
+                      type="text"
+                      placeholder="ejemplo@ejemplo.com"
+                      name="Email"
+                      value={email.Email}
+                      onChange={HandlechangeE}
+                      className={styles.formInputs} />
+                    <button className={styles.btn} onClick={send}>enviar</button>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        ) : (
+            <div className={styles.containerForm}>
+              <h4 className={styles.title}> Bienvenido/a  {logeduser.user}, vamos a   </h4>
+              <button
+                className={styles.btn1}
+                onClick={goHome}
+              >
+                <strong>Comenzar reserva <ImCalendar className={styles.icon}/></strong>
+              </button>
+            </div>
 
-)         
-}
+
+        )
+        }
+      </div>
 
     </div>
   );
