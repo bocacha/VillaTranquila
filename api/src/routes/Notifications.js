bbd4c278e08a23f12bea0377103ac1485b05ab86
Martin Gonzalez <martineduardogonzalez@gmail.com>
2021-10-14 17:13:01 -0300

    Notification

--- api/src/routes/Notifications.js
+++ api/src/routes/Notifications.js
@@ -17,25 +17,25 @@ router.get('/', async (req, res)=>{
 router.post('/', async(req, res)=>{    
     const {id, data} = req.body;
     console.log(req.body)
-    Notifications.create({
-        payment_id :id, 
-        id: data.id
+//     Notifications.create({
+//         payment_id :id, 
+//         id: data.id
         
-    })
-    .then(doneTemp=>{
-        res.status(200).json(doneTemp)
+//     })
+//     .then(doneTemp=>{
+//         res.status(200).json(doneTemp)
          
-            const {data}=doneTemp.dataValues
-            const idPago=axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`)
-            .then(res=>{
-                const {payer,title, unitPrice, card, transactionDetails,payment_method_id,date_approved,date_last_updated}=res.dataValues
-                const newPayment = axios.post('/payments/NewPayment',{payer,title, unitPrice, card, transactionDetails,payment_method_id,date_approved,date_last_updated})    
-        })
-    })
+//             const {data}=doneTemp.dataValues
+//             const idPago=axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`)
+//             .then(res=>{
+//                 const {payer,title, unitPrice, card, transactionDetails,payment_method_id,date_approved,date_last_updated}=res.dataValues
+//                 const newPayment = axios.post('/payments/NewPayment',{payer,title, unitPrice, card, transactionDetails,payment_method_id,date_approved,date_last_updated})    
+//         })
+//     })
    
    
-    .catch(error=>{ res.send(error)})
+//     .catch(error=>{ res.send(error)})
 
-});
+ });
 
 module.exports = router;
