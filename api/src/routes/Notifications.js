fdedff2990bf6812b33d447b386ae5b73da3c476
Martin Gonzalez <martineduardogonzalez@gmail.com>
2021-10-14 20:09:12 -0300

    Notifications

--- api/src/routes/Notifications.js
+++ api/src/routes/Notifications.js
@@ -28,17 +28,7 @@ router.get('/', async (req, res)=>{
 
 
 router.post('/', async(req, res)=>{  
-    console.log(req.body)
-    console.log(req.params)
-    await axios.get('https://api.mercadopago.com/v1/payments/' + req.body.data.id,{headers:{access_token: 'TEST-1809569920933245-042220-88b3cb201a62483566af682f0c59281a-246910716'}})
-    .then(res =>{
-        console.log(res)
-    })
-    // const obj={
 
-    // } 
-    
-    // axios.post('https://villatranquila.herokuapp.com/payments/newPayment', {obj})    
     res.status(200)  
  });
 
