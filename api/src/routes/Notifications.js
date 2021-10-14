321278d6021f5669882a7e5276861b2f509e8a2d
Martin Gonzalez <martineduardogonzalez@gmail.com>
2021-10-14 19:28:19 -0300

    Notifications

--- api/src/routes/Notifications.js
+++ api/src/routes/Notifications.js
@@ -14,30 +14,9 @@ router.get('/', async (req, res)=>{
 
 })
 
-router.post('/', (req, res)=>{    
-    const {id, data} = req.body;
-    console.log(req.body)
-    console.log(data)
-    console.log(id)
-    Notifications.create({
-        id :id, 
-        data: data.id
-        
-    })
-    .then(doneTemp=>{
-        res.status(200).json(doneTemp)
-         
-//             const {data}=doneTemp.dataValues
-//             const idPago=axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`)
-//             .then(res=>{
-//                 const {payer,title, unitPrice, card, transactionDetails,payment_method_id,date_approved,date_last_updated}=res.dataValues
-//                 const newPayment = axios.post('/payments/NewPayment',{payer,title, unitPrice, card, transactionDetails,payment_method_id,date_approved,date_last_updated})    
-//         })
-//     })
-   
-   
-    
-    }).catch(error=>{ res.send(error)})
+router.post('/', (req, res)=>{  
+    console.log(req.body)      
+    res.status(200)  
  });
 
 module.exports = router;
