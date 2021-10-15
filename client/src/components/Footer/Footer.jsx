a165b96b2fab6b724f1a85367fcbd2dc67cfe666
Ignacioc <63696926+IgnaC02@users.noreply.github.com>
2021-10-15 18:04:36 -0300

    Responsive Home

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -15,9 +15,9 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   const dispatch = useDispatch();
-  const [footer, setFooter] = useState({email:""});
+  const [footer, setFooter] = useState({ email: "" });
   const [errorEmail, setErrorEmail] = useState(false);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (footer === "") {
@@ -25,84 +25,96 @@ export default function Footer() {
       return;
     }
     dispatch(sendEmail(footer));
-    alert("Tu solicitud para la suscripción a la newsletter fue enviada, muchas gracias");
-    setFooter("")
+    alert(
+      "Tu solicitud para la suscripción a la newsletter fue enviada, muchas gracias"
+    );
+    setFooter("");
   };
 
   function handleChange(e) {
     setFooter({
       ...footer,
       [e.target.name]: e.target.value,
-    });  
+    });
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.arriba}>
-        <div className={styles.contacto}>
-          <span className={styles.title}> Contacto </span> <br />
-          <BiMailSend />
-          tranquilavilla79@gmail.com <br />
-          <FaMapPin /> Ubicacion
-        </div>
-        <div className={styles.links}>
-          <span className={styles.title}> Links útiles </span>  <br />
-          <Link to="/nosotros" className={styles.link}>
-            Nosotros
-          </Link>{" "}
-          <br />
-          <Link to="/contacto" className={styles.link}>
-            Contacto
-          </Link>
-          <br />
-          <Link to="/reserva" className={styles.link}>
-            Reservar
-          </Link>
-          <br />
-          <Link to="/galeria" className={styles.link}>
-            Galería
-          </Link>
-        </div>
-        <div className={styles.redes}>
-          <span className={styles.title}> Redes sociales </span> <br />
-          <Link
-            to={{ pathname: "https://www.instagram.com/?hl=es" }}
-            target="_blank"
-          >
-            <AiOutlineInstagram className={styles.redesIconos} />
-          </Link>
-          <Link to={{ pathname: "https://www.facebook.com/" }} target="_blank">
-            <AiFillFacebook className={styles.redesIconos} />
-          </Link>
-          <Link to={{ pathname: "https://www.youtube.com/" }} target="_blank">
-            <AiFillYoutube className={styles.redesIconos} />
-          </Link>
-        </div>
-        <div className={styles.newsletter}>
-          {/* {errorEmail ? alert('Tiene que ingresar su correo') : null} */}
-          <form action="" onSubmit={handleSubmit}>
-            <span className={styles.title}>
-              {" "}
-              ¡Suscríbete a nuestra NewsLetter!{" "}
-            </span>{" "}
-            <br />
-            <div>
-              <input
-               type="email"
-               name="email"
-               value={footer.email}
-               placeholder='usuario@ejemplo.com'
-               onChange={handleChange}
-               id={styles.input}
-               required
-              />
+        <div className={styles.resp}>
+          <div className={styles.test}>
+            <div className={styles.contacto}>
+              <span className={styles.title}> Contacto </span> <br />
+              <BiMailSend />
+              tranquilavilla79@gmail.com <br />
+              <FaMapPin /> Ubicacion
             </div>
-            <div>
-              <button type="submit" className={styles.btn}>
-                Suscribirme
-              </button>
+            <div className={styles.links}>
+              <span className={styles.title}> Links útiles </span> <br />
+              <Link to="/nosotros" className={styles.link}>
+                Nosotros
+              </Link>{" "}
+              <br />
+              <Link to="/contacto" className={styles.link}>
+                Contacto
+              </Link>
+              <br />
+              <Link to="/reserva" className={styles.link}>
+                Reservar
+              </Link>
+              <br />
+              <Link to="/galeria" className={styles.link}>
+                Galería
+              </Link>
             </div>
-          </form>
+            <div className={styles.redes}>
+              <span className={styles.title}> Redes sociales </span> <br />
+              <Link
+                to={{ pathname: "https://www.instagram.com/?hl=es" }}
+                target="_blank"
+              >
+                <AiOutlineInstagram className={styles.redesIconos} />
+              </Link>
+              <Link
+                to={{ pathname: "https://www.facebook.com/" }}
+                target="_blank"
+              >
+                <AiFillFacebook className={styles.redesIconos} />
+              </Link>
+              <Link
+                to={{ pathname: "https://www.youtube.com/" }}
+                target="_blank"
+              >
+                <AiFillYoutube className={styles.redesIconos} />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.newsletter}>
+            {/* {errorEmail ? alert('Tiene que ingresar su correo') : null} */}
+            <form action="" onSubmit={handleSubmit}>
+              <span className={styles.title}>
+                {" "}
+                ¡Suscríbete a nuestra NewsLetter!{" "}
+              </span>{" "}
+              <br />
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  value={footer.email}
+                  placeholder="usuario@ejemplo.com"
+                  onChange={handleChange}
+                  id={styles.input}
+                  required
+                />
+              </div>
+              <div>
+                <button type="submit" className={styles.btn}>
+                  Suscribirme
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
       <hr className={styles.hr} />
